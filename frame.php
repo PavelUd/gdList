8fae6c433f384ae4162b4ff81773922e41f313fb
PavelUd <paveludin4@gmail.com>
2023-07-09 23:37:21 +0600

    second

--- frame.php
+++ frame.php
@@ -1,14 +1,24 @@
 <?php
 Class Frame
 {
-    function get_header()
+    function get_header($page)
     {
+        $array = array(
+            "MainList" => "Main List",
+            "LegacyList" => "Legacy List",
+            "ExtendedList" => "Extended List"
+        );
         $result = ' <header class="site-header sticky-top py-3">
   <nav class="container d-flex flex-column flex-md-row justify-content-between" style="font-size: large">
-    <div class="btn-group col-8 mx-auto" role="group" aria-label="Basic outlined example">
-      <a type="button" class="btn btn-outline-dark" href="?page=MainList"><b>Main List</b></a>
-      <a type="button" class="btn btn-outline-dark" href="?page=LegacyList"><b>Legacy List</b></a>
-      <a type="button" class="btn btn-outline-dark"><b>Extended List</b></a>
+    <div class="btn-group col-8 mx-auto" role="group" aria-label="Basic outlined example">';
+        foreach ($array as $key => $value) {
+            if ($key == $page) {
+                $result .= ' <a type="button" class="btn btn-outline-dark active" href="?page=' . $key . '"><b>' . $value . '</b></a>';
+            } else {
+                $result .= '<a type="button" class="btn btn-outline-dark" href="?page=' . $key . '"><b>' . $value . '</b></a>';
+            }
+        }
+      $result .= '
     </div>
     <a style="text-decoration: none; font-family: Georgia" class="btn btn-outline-dark" href="#"><b>Sign up</b></a>
     </nav>
