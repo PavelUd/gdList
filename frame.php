15125b9a8fdaf0873f25fea6b2754abae0b11b50
PavelUd <paveludin4@gmail.com>
2023-07-15 19:26:09 +0600

    добавлена возможность сортироки карточек по таерам

--- frame.php
+++ frame.php
@@ -9,7 +9,7 @@ Class Frame
             "ExtendedList" => "Extended List"
         );
         $result = ' <header class="site-header sticky-top py-3">
-  <nav class="container d-flex flex-column flex-md-row justify-content-between" style="font-size: large">
+  <nav class="container d-flex flex-column flex-md-row justify-content-between" style="font-size: large; margin-bottom: 1rem">
     <div class="btn-group col-8 mx-auto" role="group" aria-label="Basic outlined example">';
         foreach ($array as $key => $value) {
             if ($key == $page) {
@@ -21,8 +21,16 @@ Class Frame
       $result .= '
     </div>
     <a style="text-decoration: none; font-family: Georgia" class="btn btn-outline-dark" href="#"><b>Sign up</b></a>
-    </nav>
-</header>';
+    </nav>';
+      $result .= ' <nav class="container d-flex flex-column flex-md-row justify-content-between" style="font-size: large; margin-bottom: 1rem"><div class="btn-group col-12 mx-auto" role="group" aria-label="Basic outlined example" id="header" onclick="get_info(event)">
+                    <button type="button" class="btn btn-outline-danger" style="font-weight: bold">' . $value . '</button>
+                    <button type="button" class="btn btn-outline-gold" name = "gold" style="font-weight: bold">gold</button>
+                    <button type="button" class="btn btn-outline-dark" style="font-weight: bold">' . $value . '</button>
+                    <button type="button" class="btn btn-outline-primary" style="font-weight: bold">' . $value . '</button>
+                    <button type="button" class="btn btn-outline-diamond" name="diamond" style="font-weight: bold">diamond</button>
+                    <button type="button" class="btn btn-outline-success"  style="font-weight: bold">' . $value . '</button>
+                    <button type="button" class="btn btn-outline-secondary" style="font-weight: bold">' . $value . '</button>
+</div></nav></header>';
         return $result;
     }
 }
--- main_list.php
+++ main_list.php
@@ -3,10 +3,10 @@ Class MainList
 {
     function get_header()
     {
-        $result = '<div class="col-md-6" style="margin-left: 1.5rem">';
+        $result = '<div class="col-md-6" style="margin-left: 1.5rem"><div id="TreeCard">';
         for ($i = 0;$i < 10; $i++)
         {
-            $result .= '<a href="?page=LegacyList" class="card mb-3" style="max-width: 540px; margin-left: 1.5rem; margin-top: 1.5rem; text-decoration: none; color: black"><div class = "mini-card">
+            $result .= '<a href="?page=LegacyList" class="card" style="max-width: 540px; margin-left: 1.5rem; margin-top: 1.5rem; text-decoration: none; color: black " name="diamond" ><div class = "mini-card" data-aos="fade-down">
           <div class="row g-0">
             <div class="col-md-3">
               <img src="css/img/h.jpg" class="img-fluid rounded-start" style="margin: 1rem">
@@ -21,7 +21,21 @@ Class MainList
 </div>
 </a>';
         }
-        $result .= '</div>';
+        $result .= '<a href="?page=LegacyList" class="card" style="max-width: 540px; margin-left: 1.5rem; margin-top: 1.5rem; text-decoration: none; color: black " name="gold"><div class = "mini-card">
+          <div class="row g-0">
+            <div class="col-md-3">
+              <img src="css/img/h.jpg" class="img-fluid rounded-start" style="margin: 1rem">
+            </div>
+    <div class="col-md-4">
+      <div class="card-body">
+        <h5 class="card-title">Заголовок</h5>
+        <p class="card-text">fgf</p>
+      </div>
+    </div>
+  </div>
+</div>
+</a>';
+        $result .= '</div></div>';
         return $result;
     }
 }
