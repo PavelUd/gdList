76361ba626a2e6fe154f95f2eed7338735b9d175
PavelUd <paveludin4@gmail.com>
2023-08-02 14:10:06 +0500

    big update

--- card.php
+++ card.php
@@ -1,6 +1,82 @@
 <?php
-
-class card
+class Card
 {
+    private $id;
+    private $info;
+    private $records;
+    private $fps;
+    public function __construct()
+    {
+        $db = new \gdlist\www\Db();
+        $this->id = $_REQUEST["id"];
+        $params = array(
+            'id' => $this->id
+        );
+        $this->records = $db->get_rows("
+            select lr.*, m.name from level_records lr 
+        join members m on lr.player_id = m.id   
+         where level_id = :id", $params);
+        $this->info = $db->get_row("select * from levels where id = :id", $params);
+        $this->fps =  $this->info["FPS"] ?  $this->info["FPS"] : "Any";
+    }
+    function get_media()
+    {
+        extract($this->info);
+        $points = $this->get_level_points();
+        $result = '<div style="margin-top: 1rem; margin-left: 1rem; text-align: center"><h2 style="font-weight: bold;">'.$name.'</h2>';
+        $result .= '<h4 style="font-weight: bold;">'.$Account.'</h4>';
+        $result .= '<div style="text-align: center; margin-top: 1rem">';
+        $result .= '<video
+        id="vid'.$this->id.'"
+        class="video-js  vjs-theme-fantasy"
+        controls
+        width="640" height="324"
+        data-setup=\'{
+        "techOrder": ["youtube"], "sources": [{
+            "type": "video/youtube", "src": "https://www.youtube.com/watch?v='.$link.'"}] }\'
+>
+</video></div>';
 
+        $result .= ' <div style=" margin-top: 1rem; justify-content: center; display: flex"><div class="col-md-6"><table class="table table-borderless">
+                    <tr>
+                    <th>FPS: '.$this->fps.'</th>
+                    <th>Points: '.$points.'</th>
+                    </tr>
+                    </table></div></div>';
+        $result .= $this->get_results_table();
+        return $result;
+    }
+    private function get_results_table()
+    {
+        $table = '<h2 style="font-weight: bold; margin-top: 1rem">Records</h2>
+                        <div style=" margin-top: 1rem; justify-content: center; display: flex">
+                    <div class="col-md-6">
+                    <table class="table table-striped table-hover">
+                    <thead>
+                    <tr>
+                        <th scope="col">Record Holder</th>
+                        <th scope="col">Video Proof</th>
+                   </tr>
+                    </thead>
+                    <tbody>
+                    <tr>';
+        for ($i = 0; $i < count($this->records);$i++)
+        {
+            $link = $this->records[$i]["video_proof"] ? '<a href="'.$this->records[$i]["video_proof"].'">':"";
+            $table .= '<td>'.$this->records[$i]["name"].'</td>
+                    <td>'.$link.'video</a></td>
+                    </tr>';
+                    }
+        $table .='</tbody>
+                    </table>
+                    </div></div></div>';
+        return $table;
+    }
+    private function get_level_points()
+    {
+        $jsonFilePath = 'level_types.json';
+        $jsonString = file_get_contents($jsonFilePath);
+        $levelTypes = json_decode($jsonString, true);
+        return $levelTypes[$this->info["type"]];
+    }
 }
--- frame.php
+++ frame.php
@@ -8,30 +8,55 @@ Class Frame
             "LegacyList" => "Legacy List",
             "ExtendedList" => "Extended List"
         );
-        $result = ' <header class="site-header sticky-top py-3">
-  <nav class="container d-flex flex-column flex-md-row justify-content-between" style="font-size: large; margin-bottom: 1rem">
+        $result = '
+  <nav class="container d-flex flex-column flex-md-row justify-content-between" style="font-size: large; margin-top: 1rem">
     <div class="btn-group col-8 mx-auto" role="group" aria-label="Basic outlined example">';
         foreach ($array as $key => $value) {
-            if ($key == $page) {
-                $result .= ' <a type="button" class="btn btn-outline-dark active" href="?page=' . $key . '"><b>' . $value . '</b></a>';
+            if ($key == $page["page"]) {
+                $result .= ' <a type="button" class="btn btn-outline-primary active" href="?page=' . $key . '"><b>' . $value . '</b></a>';
             } else {
-                $result .= '<a type="button" class="btn btn-outline-dark" href="?page=' . $key . '"><b>' . $value . '</b></a>';
+                $result .= '<a type="button" class="btn btn-outline-primary" href="?page=' . $key . '"><b>' . $value . '</b></a>';
             }
         }
-
+        $types = array(
+            "exodium" => array("name" => "Exodium",
+                            "type" => "btn btn-outline-dark"),
+            "legend" => array("name" => "Legend",
+                            "type" => "btn btn-outline-secondary"),
+            "amethyst" => array("name" => "Amethyst",
+                            "type" => "btn btn-outline-amethyst"),
+            "diamond" => array("name" => "Diamond",
+                            "type" => "btn btn-outline-diamond"),
+            "ruby" => array("name" => "Ruby",
+                            "type" => "btn btn-outline-ruby"),
+            "emerald" => array("name" => "Emerald",
+                            "type" => "btn btn-outline-emerald"),
+            "gold" => array("name" => "Gold",
+                            "type" => "btn btn-outline-gold"),
+            "silver" => array("name" => "Silver",
+                            "type" => "btn btn-outline-silver"),
+            "bronze" => array("name" => "Bronze",
+                            "type" => "btn btn-outline-bronze"),
+            "rock" => array("name" => "Rock",
+                            "type" => "btn btn-outline-rock"),
+        );
       $result .= '
     </div>
-    <button style="text-decoration: none; font-family: Georgia" class="btn btn-outline-dark" data-target="#addSingModal" data-toggle="modal"><b>Sign up</b></button>
+    <button style="text-decoration: none; font-family: Georgia" class="btn btn-outline-primary" data-target="#addSingModal" data-toggle="modal"><b>Sign up</b></button>
     </nav>';
-      $result .= ' <nav class="container d-flex flex-column flex-md-row justify-content-between" style="font-size: large; margin-bottom: 1rem"><div class="btn-group col-12 mx-auto" role="group" aria-label="Basic outlined example" id="header" onclick="get_info(event)">
-                    <button type="button" class="btn btn-outline-danger" style="font-weight: bold">' . $value . '</button>
-                    <button type="button" class="btn btn-outline-gold" name = "gold" style="font-weight: bold">Gold</button>
-                    <button type="button" class="btn btn-outline-dark" style="font-weight: bold">' . $value . '</button>
-                    <button type="button" class="btn btn-outline-primary" style="font-weight: bold">' . $value . '</button>
-                    <button type="button" class="btn btn-outline-diamond" name="diamond" style="font-weight: bold">Diamond</button>
-                    <button type="button" class="btn btn-outline-success"  style="font-weight: bold">' . $value . '</button>
-                    <button type="button" class="btn btn-outline-secondary" style="font-weight: bold">' . $value . '</button>
-</div></nav></header>';
+      $result .= ' <nav class="container d-flex flex-column flex-md-row justify-content-between" style="font-size: large; margin-top: 1rem"><div class="btn-group col-12 mx-auto" role="group"  id="header" onclick="get_info(event)">';
+      $page["page"] = (isset($array[$page["page"]])) ? $page["page"] : 'MainList';
+      foreach ($types as $type => $typeArray)
+      {
+          if (isset($page["type"]) && $page["type"] == $type)
+          {
+              $result .= '<a type="button" class="'.$typeArray["type"].' active" name = "gold" href="?page='.$page["page"].'&type='.$type.'" style="font-weight: bold">'.$typeArray["name"].'</a>';
+          }
+          else {
+              $result .= '<a type="button" class="' . $typeArray["type"] . '" name = "gold" href="?page=' . $page["page"] . '&type=' . $type . '" style="font-weight: bold">' . $typeArray["name"] . '</a>';
+          }
+      }
+$result .= '</div></nav>';
         $result .= '<div class="modal fade" id="addSingModal" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
